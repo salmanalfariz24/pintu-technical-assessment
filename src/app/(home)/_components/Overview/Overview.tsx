92331901b21a0973083afef184ee33f027e093e9
Salman Al Fariz <salmanalfariz24@gmail.com>
2024-07-19 09:02:30 +0700

    feat(home): remove pagination

--- src/app/(home)/_components/Overview/Overview.tsx
+++ src/app/(home)/_components/Overview/Overview.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import { Fragment } from 'react';
 import { flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table';
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import type { AssetCollectionDataType } from '@/model/assets';
 
 import useDataSource from './_hooks/use-data-source';
-import useTableFunctions from './_hooks/use-table-functions';
-
-import Pagination from './Pagination';
 
 import { columns } from './data-table';
 
@@ -26,56 +22,46 @@ function Overview(props: OverviewPropsType) {
     getPaginationRowModel: getPaginationRowModel(),
     initialState: {
       pagination: {
-        pageSize: 25,
+        pageSize: 100,
       },
     },
   });
 
-  const { isNextPageDisabled, isPreviousPageDisabled, onGoToNextPage, onGoToPreviousPage } = useTableFunctions(table);
-
   return (
-    <Fragment>
-      <div className="rounded-md border mt-4">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map(headerGroup => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map(header => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                    </TableHead>
-                  );
-                })}
+    <div className="rounded-md border my-4">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map(headerGroup => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map(row => (
+              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                {row.getVisibleCells().map(cell => (
+                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                ))}
               </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map(row => (
-                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                  {row.getVisibleCells().map(cell => (
-                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-      <Pagination
-        isNextPageDisabled={isNextPageDisabled}
-        isPreviousPageDisabled={isPreviousPageDisabled}
-        onGoToNextPage={onGoToNextPage}
-        onGoToPreviousPage={onGoToPreviousPage}
-      />
-    </Fragment>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
 
