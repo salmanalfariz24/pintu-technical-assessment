070323a8db141fe2f399163417288a0f027d4e1f
Salman Al Fariz <salmanalfariz24@gmail.com>
2024-07-19 08:40:30 +0700

    fix(currencies/[id]/page): remove mock

--- src/app/currencies/[id]/page.tsx
+++ src/app/currencies/[id]/page.tsx
@@ -4,8 +4,6 @@ import { normalizeSingleAsset } from '@/repository/assets/normalizer';
 
 import Main from '@/components/atoms/Main';
 
-import type { AssetDataType } from '@/model/assets';
-
 import Information from '../_components/Information';
 import Graph from '../_components/Graph';
 
@@ -15,30 +13,17 @@ interface PagePropsType {
   };
 }
 
-const MOCK: AssetDataType = {
-  id: 'bitcoin',
-  rank: 1,
-  symbol: 'BTC',
-  name: 'Bitcoin',
-  price: '63823.9307508367016017',
-  detailURL: '/currencies/bitcoin',
-  marketCap: '1259081551796.6017147481102157',
-  supply: '19727421.0000000000000000',
-  maxSupply: '21000000.0000000000000000',
-  changePercentage: '-0.9925573213666201',
-};
-
 async function Page(props: PagePropsType) {
   const { params } = props;
-  // const data = await getAssetsByID(params.id);
-  // const assets = normalizeSingleAsset(data);
+  const data = await getAssetsByID(params.id);
+  const assets = normalizeSingleAsset(data);
 
   return (
     <Main className="flex justify-center mt-3" padding="dense">
       <div className="flex flex-row w-full h-full gap-3">
-        <Information {...MOCK} />
+        <Information {...assets} />
         <Suspense>
-          <Graph id={MOCK.id} />
+          <Graph id={assets.id} />
         </Suspense>
       </div>
     </Main>
